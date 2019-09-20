e8246c6ae9b18d866517abd3646f7009abc12e97
unknown <kensixu@hotmail.com>
2019-09-20 16:23:35 -0400

    add check question

--- magic_eight.py
+++ magic_eight.py
@@ -1,3 +1,13 @@
 def my_function():
     question = input("What is your question?")
     return question
+
+while True:
+    ques = my_function()
+    if ques[-1] == "?":
+        print("Yes, it's a question.")
+        break
+    elif ques == "quit":
+        break
+    else:
+        print("I'm sorry, I can only answer questions.")
